b9c5f310f8aa86cdc2c36d11742861602f62877b
NoahKoo <103397887+NoahKoo@users.noreply.github.com>
2022-10-02 08:16:04 +0900

    Middlewares(2)

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
+const logger = (req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 };
@@ -13,8 +13,7 @@ const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
 
-app.use(gossipMiddleware);
-app.get("/", handleHome);
+app.get("/", logger, handleHome);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
