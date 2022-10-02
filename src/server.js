4e65ebf360426f2c56aed3b7e3a1665615ac8d79
NoahKoo <103397887+NoahKoo@users.noreply.github.com>
2022-10-02 19:08:28 +0900

    Using Express && Routers

--- src/server.js
+++ src/server.js
@@ -1,19 +1,33 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+const globalRouter = express.Router();
 
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
+const handleHome = (req, res) => res.send("Home");
 
-app.get("/", logger, handleHome);
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
